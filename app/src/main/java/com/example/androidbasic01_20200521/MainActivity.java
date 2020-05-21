b4e2fe311efb34e570794b8b4282e84712731226
weekyeon <weekyeon@gmail.com>
2020-05-21 11:31:26 +0900

    binding

--- app/src/main/java/com/example/androidbasic01_20200521/MainActivity.java
+++ app/src/main/java/com/example/androidbasic01_20200521/MainActivity.java
@@ -1,22 +1,39 @@
 package com.example.androidbasic01_20200521;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.view.View;
+
+import com.example.androidbasic01_20200521.databinding.ActivityMainBinding;
+
+import static com.example.androidbasic01_20200521.R.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, layout.activity_main);
         setupEvents();
         setValues();
     }
 
 //    이벤트 처리 코드 모아두는 곳
     void setupEvents() {
+        binding.goToFirstBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+//                FirstActivity로 이동.
+
+            }
+        });
     }
 
 //    화면에 나와야할 데이터 세팅 코드 모아두는 곳
